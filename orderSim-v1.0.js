83be6eb54a7a723cd0509ec1c83ad34f0bb5ea69
Bryan Woodard <bwoodard40@gmail.com>
2018-06-20 14:08:47 -0700

    Sync
    Syncs up order app code with snippet

--- orderSim-v1.0.js
+++ orderSim-v1.0.js
@@ -1,24 +1,18 @@
 /*
-version 1.0 - Stable and working based on predefined product values 
-To be included in later versions:
-	- send order based on load rule
-	- define client specific product vars
-	- send order based on tag#
+version 1.1
+To be included in next release:
+	- optimize code, let redundancies
+	- 
 */
-
-/*Random Order value generator
-	Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5) + Math.floor(Math.random()*1000)
-hats
-
-*/
-
-
 
 window.orderSim = window.orderSim || {
-	send: function(type, prodNum, loadRule, specificProductValuesArray){
+	send: function(type, prodNum, tagArray, objJoin, specProdStrings, specProdNums ){
 		if ((type == "link" || type == "view") && !isNaN(prodNum)){
-			var obj = this.buildProdVals(prodNum) // change all "orderSim" to this
-			utag[type](obj);
+			var obj = this.buildProdVals(prodNum, specProdStrings, specProdNums) // changed to allow for specific prod values
+			if(objJoin){
+				Object.assign(obj, objJoin);
+			}
+			utag[type](obj,null,tagArray);
 		}else{
 			console.log("Invalid utag tracking method");
 		}
@@ -43,18 +37,17 @@ window.orderSim = window.orderSim || {
 			for(var i = 0; i<numP; i++){
 				arr.push((Math.random()*100).toFixed(2));
 			}
-			return arr;
 		}else if(!numP && numQ){
 			for(var i = 0; i<numQ; i++){
 				arr.push(Math.round((Math.random()*10 + 1)).toString()); 
 			}
-			return arr;
 		}
-		
+		return arr;
 	},
-	buildProdVals: function(num, prodVals){
-		if(!prodVals){
-			var obj = {};
+	buildProdVals: function(num, prodString, prodNum){
+		var obj = {};
+		if(!prodString && !prodNum){
+			
 			var prds = ["product_id", "product_sku", "product_brand", "product_category","product_name"]
 			
 			var getPrice = function(p, q){
@@ -74,9 +67,28 @@ window.orderSim = window.orderSim || {
 			obj.order_subtotal = getPrice(obj.product_price, obj.product_quantity);
 			obj.order_total = (parseFloat(obj.order_subtotal) + parseFloat(obj.order_subtotal * .08)).toFixed(2);
 			obj.order_id = this.buildString(1).toString();
-			return obj;
+			//return obj;
 		}else{
-			//do something with the specific product array values
+			// finish here!!!!!!not  complete func,
+			// How can we determine prod quant and price values
+			var price = 0;
+			for (let i = 0;i<prodString.length;i++){
+				obj[prodString[i]] = this.buildString(num);
+			}
+			for (let i = 0;i<prodNum.length;i++){
+				if(prodNum[i].includes("price")){
+					obj[prodNum[i]] = this.buildPQ(num);
+				}else if (prodNum[i].includes("quan")){
+					obj[prodNum[i]] = this.buildPQ(null, num);
+				}
+			}
+			for(thing in obj){
+				// something with obj
+			}
+			obj.order_id = this.buildString(1).toString();
+			//add functionaliy to include subtotal, total and way to dynamically set these vals 
+			//return obj;
 		}
+		return obj;
 	}		
 }
